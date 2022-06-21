872d023ff17960ba9346447de7e43a63365e2371
suarezfer1712 <suarezfer1712@gmail.com>
2022-06-22 00:49:39 +0100

    added a scroll bar to the beer card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -8,7 +8,7 @@ const Card = ({beer}) => {
   return (
     <article className="beer-card">
       <div className={id%2===0?"bg-color--yellow":"bg-color--red"}>
-          <h2 className="beer-card-name">{name}</h2>
+          <h2 className="beer-card__name">{name}</h2>
           <div className="beer-card__img"style={{backgroundImage: `url(${image_url})`}}/>
           <button className='beer-card__alcohol'>{abv}</button>
           <div className='beer-card__info'>
